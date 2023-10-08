bb6fb091f6e5713033b7f7e837148a6b7a8a721f
maruf02 <rashiduzzaman99@gmail.com>
2023-10-08 12:22:17 +0600

    Note: fetch data from json and show into services section in homepage and send data individually card details page without private router

--- src/Components/ServiceDetails/ServiceDetails.jsx
+++ src/Components/ServiceDetails/ServiceDetails.jsx
@@ -7,10 +7,17 @@ const ServiceDetails = () => {
   const idInt = parseInt(id);
   const card = cards.find((card) => card.id === idInt);
   console.log(card.title);
-  const { title } = card;
+  const { title, img, price, duration, long_desc } = card;
   return (
-    <div>
-      <h2>This is service details page:{title}</h2>
+    <div className="container mx-auto text-black flex flex-col items-center ">
+      <img src={img} alt="" className="w-3/4 lg:h-[400px]" />
+      <h2 className="text-5xl font-bold  pt-10 underline">Title: {title}</h2>
+      <h2 className="text-3xl pt-5">Price: {price}</h2>
+      <h2 className="text-3xl pt-5">Duration: {duration}</h2>
+      <p className="text-lg py-5 text-justify">
+        <span className="text-2xl font-bold text-center">About Event: </span>
+        {long_desc}
+      </p>
     </div>
   );
 };
